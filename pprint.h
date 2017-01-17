833a45f7d6c89a59ab2404fefefd1e2ce6a44970
sasdf <asdf79852@gmail.com>
2017-01-17 23:57:21 +0800

    Update pprint.h

--- pprint.h
+++ pprint.h
@@ -8,8 +8,9 @@
 
 #define PP_C_RESET        "\x1b[0m"
 #define PP_C_BLACK_B      "\x1b[30;1m"
+#define PP_C_RED_B        "\x1b[31;1m"
 #define PP_C_GREEN_B      "\x1b[32;1m"
-#define PP_C_RED_B        "\x1b[34;1m"
+#define PP_C_BLUE_B       "\x1b[34;1m"
 #define PP_C_CYAN_B       "\x1b[36;1m"
 
 #define PP_FILE_FMT           PP_C_BLACK_B " @ %s:%d"
@@ -18,13 +19,21 @@
 #define PP_POSTFIX_FMT  " " PP_FILE_FMT PP_C_RESET "\n"
 #define PP_POSTFIX_ARG  PP_FILE_ARG
 
-#define INFStr(str)          fprintf(stderr, PP_C_GREEN_B "[INF]" PP_C_RESET " " "%s"   PP_POSTFIX_FMT, str,    PP_POSTFIX_ARG)
-#define INF(format, args...) fprintf(stderr, PP_C_GREEN_B "[INF]" PP_C_RESET " " format PP_POSTFIX_FMT, ##args, PP_POSTFIX_ARG)
-#define INFStart()           fprintf(stderr, PP_C_GREEN_B "[INF]" PP_C_RESET " ")
+#define OPTStr(str)          fprintf(stderr, PP_C_RESET   "[OPT]" PP_C_RESET " " "%s"   PP_POSTFIX_FMT, str,    PP_POSTFIX_ARG)
+#define OPT(format, args...) fprintf(stderr, PP_C_RESET   "[OPT]" PP_C_RESET " " format PP_POSTFIX_FMT, ##args, PP_POSTFIX_ARG)
+#define OPTStart()           fprintf(stderr, PP_C_RESET   "[OPT]" PP_C_RESET " ")
+#define OPTEnd()             fprintf(stderr, PP_POSTFIX_FMT, PP_POSTFIX_ARG)
+#define INFStr(str)          fprintf(stderr, PP_C_CYAN_B  "[INF]" PP_C_RESET " " "%s"   PP_POSTFIX_FMT, str,    PP_POSTFIX_ARG)
+#define INF(format, args...) fprintf(stderr, PP_C_CYAN_B  "[INF]" PP_C_RESET " " format PP_POSTFIX_FMT, ##args, PP_POSTFIX_ARG)
+#define INFStart()           fprintf(stderr, PP_C_CYAN_B  "[INF]" PP_C_RESET " ")
 #define INFEnd()             fprintf(stderr, PP_POSTFIX_FMT, PP_POSTFIX_ARG)
-#define DBGStr(str)          fprintf(stderr, PP_C_CYAN_B  "[DBG]" PP_C_RESET " " "%s"   PP_POSTFIX_FMT, str,    PP_POSTFIX_ARG)
-#define DBG(format, args...) fprintf(stderr, PP_C_CYAN_B  "[DBG]" PP_C_RESET " " format PP_POSTFIX_FMT, ##args, PP_POSTFIX_ARG)
-#define DBGStart()           fprintf(stderr, PP_C_CYAN_B  "[DBG]" PP_C_RESET " ")
+#define ACKStr(str)          fprintf(stderr, PP_C_GREEN_B "[ACK]"            " " "%s"   PP_POSTFIX_FMT, str,    PP_POSTFIX_ARG)
+#define ACK(format, args...) fprintf(stderr, PP_C_GREEN_B "[ACK]"            " " format PP_POSTFIX_FMT, ##args, PP_POSTFIX_ARG)
+#define ACKStart()           fprintf(stderr, PP_C_GREEN_B "[ACK]"            " ")
+#define ACKEnd()             fprintf(stderr, PP_POSTFIX_FMT, PP_POSTFIX_ARG)
+#define DBGStr(str)          fprintf(stderr, PP_C_BLUE_B  "[DBG]"            " " "%s"   PP_POSTFIX_FMT, str,    PP_POSTFIX_ARG)
+#define DBG(format, args...) fprintf(stderr, PP_C_BLUE_B  "[DBG]"            " " format PP_POSTFIX_FMT, ##args, PP_POSTFIX_ARG)
+#define DBGStart()           fprintf(stderr, PP_C_BLUE_B  "[DBG]"            " ")
 #define DBGEnd()             fprintf(stderr, PP_POSTFIX_FMT, PP_POSTFIX_ARG)
 #define ERRStr(str)          fprintf(stderr, PP_C_RED_B   "[ERR]"            " " "%s"   PP_POSTFIX_FMT, str,    PP_POSTFIX_ARG)
 #define ERR(format, args...) fprintf(stderr, PP_C_RED_B   "[ERR]"            " " format PP_POSTFIX_FMT, ##args, PP_POSTFIX_ARG)
